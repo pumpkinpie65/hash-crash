59697d0084e10b0d16aca8d33a3719f7b29264c7
pumpkinpie65 <pumpkinpie65@live.com>
2014-05-22 09:54:05 -0700

    created HashCrash.java

---
+++ HashCrash/src/hashcrash/HashCrash.java
@@ -0,0 +1,24 @@
+/*
+ * David Zima
+ * created 5.22.14
+ * CS 182 Lab Project 6 Hash Crash
+ * 
+ * 
+ */
+
+package hashcrash;
+
+/**
+ *
+ * @author David
+ */
+public class HashCrash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
